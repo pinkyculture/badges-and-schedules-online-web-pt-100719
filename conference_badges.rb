ec970ea270f766beae3a6e1feb823f1cd83ab3c0
daniel arzayus <randomblue1@yahoo.com>
2019-10-24 03:35:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,5 +19,5 @@ end
 
 def printer(names)
 batch_badge_creator(names).each {|badge_message| puts badge_message }
-assign_rooms(names).each {|room.assignment| puts room assignment}
+assign_rooms(names).each {|room.assignment| puts room assignment }
 end

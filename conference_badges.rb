caead24100fdd94e6b5c73d8b75c9a959e924d2f
daniel arzayus <randomblue1@yahoo.com>
2019-10-24 02:59:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,3 +4,13 @@ end
 def batch_badge_creator (name)
   name.each do |names|
 array = []
+return "Hello, my name is #{name}."
+end
+def assign_rooms
+numbers = []
+names = []
+  return "Hello, #{names}! You'll be assigned to room #{numbers}!"
+end
+def printer
+  puts names + numbers
+end

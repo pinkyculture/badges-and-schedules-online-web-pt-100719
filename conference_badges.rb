10465b16f5371ef1230324b27fb0f8691d20198a
daniel arzayus <randomblue1@yahoo.com>
2019-10-24 02:57:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker (badge)
+  badge = "Hello, my name is #{name}."
+end
+def batch_badge_creator (name)
+array = []
+return "Hello, my name is #{name}."
+end
+def assign_rooms
+numbers = []
+names = []
+  return "Hello, #{names}! You'll be assigned to room #{numbers}!"
+end
+def printer
+  puts names + numbers
+end
